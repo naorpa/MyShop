3c346465adda637e1e2d71aab2f2c758d44dfa45
Naor Pashy <naorpashay94@gmail.com>
2021-08-30 09:51:43 +0300

    commting chaces to myshop

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+            //we put the defaultConnection becuse in the web.config file we have instance of defaultConnection name that tell him wich db to connect
+        {
+
+        }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
